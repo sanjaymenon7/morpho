011cfe7ea7174c1935a82d1b9927d9bd77dcfcca
sanjaymenon <sanjayzzzz@gmail.com>
2015-11-08 23:58:19 +0530

    Adding jsx page hello world after installing react

---
+++ views/error.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var ErrorMessage = React.createClass({
+    render: function() {
+        return <div><h2>An error has occured</h2></div>;
+    }
+});
+
+module.exports = ErrorMessage;
