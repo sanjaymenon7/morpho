205737f28b2c56e9d254bcebf74b6843bafa9a62
Ahmed <mahmedk91@gmail.com>
2015-11-13 02:32:28 +0100

    Import Service -      0. Remember to always start mongodb server manually. I will try to find a way to start it automatically with "npm start" 1. Go to http://localhost:3000/upload 2. Choose the new data.csv file in recent commit 3. Give a table name. (Please give table name "phonedata" atleast once so that you can see the data in console the next time you start the app)     @Togi you can take some code from phoneData.js to create export service    @Sanjay your data is again in exports.foneSpecs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var phoneInfo = require('../data/phoneData')
+var phoneInfo = require('../data/phoneData');
+
+// Multer - A lib to manage uploads
+var multer  = require('multer');
+
+// For Linux, Mac - Please change the path of uploads to same folder as MongoDB
+var upload = multer({ dest: 'c:/data/csv' })
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +15,25 @@ router.get('/', function(req, res, next) {
  //res.send(phoneInfo.phoneSpecs[0].CompanyName);
  //console.log(phoneInfo.phoneSpecs[0]);
 });
+
+// Upload form for now - Later we need to create a view
+router.get('/upload', function(req, res) {
+  res.send('<form action="/create-table" method="post" enctype="multipart/form-data"><input name="data_file" type="file">Table Name<input name="tablename" type="text"><input type="Submit" value="Upload"></form>');
+});
+
+// Import Service - Upload csv to create a new table
+router.post('/create-table',upload.single('data_file'), function(req, res, next) {
+  exec('mongoimport --db morphologicalrecommender --collection '+req.body.tablename+' --type csv --headerline --file C:data\\csv\\'+req.file.filename, function(error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+          console.log('exec error: ' + error);
+      }
+      res.send(req.body.tablename+' Table Created!');
+  });
+});
+
+// TODO: @Togi Export Service - We need a param/attribute to export one table. See phoneData.js for more comments
 
 module.exports = router;
 
