aaabccbebc536b22e2155eb9783cf86523111a84
sanjaymenon <sanjayzzzz@gmail.com>
2015-11-14 00:14:23 +0530

    Changing MongoDb path and package changes

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/upload', function(req, res) {
 
 // Import Service - Upload csv to create a new table
 router.post('/create-table',upload.single('data_file'), function(req, res, next) {
-  exec('mongoimport --db morphologicalrecommender --collection '+req.body.tablename+' --type csv --headerline --file C:data\\csv\\'+req.file.filename, function(error, stdout, stderr) {
+  exec('mongoimport --db morphologicalrecommender --collection '+req.body.tablename+' --type csv --headerline --file C:\\data\\csv\\'+req.file.filename, function(error, stdout, stderr) {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
       if (error !== null) {
