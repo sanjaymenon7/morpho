12dd8078ce3a40785dbfb5a63a339685d1551a3d
Ahmed <mahmedk91@gmail.com>
2015-11-25 14:58:04 +0100

    Remaining backend services for Sanjay -
    Service to populate performance columns - GET - http://localhost:3000/coloumnpreprocessor/getPerfColList
    Service to set performance and filter columns - POST - http://localhost:3000/coloumnpreprocessor/setDataSet
    
    Also, redirected to Kaushik's page on success.

--- routes/coloumnpreprocessor.js
+++ routes/coloumnpreprocessor.js
@@ -1,33 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('morphologicalrecommender', server);
+
+// Multer - A lib to manage uploads
+var multer  = require('multer');
 
 /* GET start start page. */
 router.get('/', function(req, res, next) {
-    res.render('coloumnpreprocessor');
+     if (!req.session.loggedIn) {
+        res.render("start");
+      } else if (!req.session.tableSet) {
+        res.render("datasourceselection");
+      } else {
+        res.render('coloumnpreprocessor');
+      }  
 });
 
-
-var datasetList =
-    [
-        {
-            "id": "pixeldensity",
-            "text": "Pixel Density",
-        },
-        {
-            "id": "builtinstorage",
-            "text": "Built-in Storage"
-        },
-        {
-            "id": "batterycapacity",
-            "text": "Battery Capacity"
-        }
-    ]
-
+// Service to populate performance coloumn dropdown
 router.get('/getPerfColList',function(req,res,next){
-    res.json(datasetList)
+    if (!req.session.loggedIn || !req.session.tableSet) {
+        responseJson.status=false;
+        res.json(responseJson);
+      } else {
+        db.open(function(err, db) {
+            if(!err) {
+                var query = 'db.'+req.session.table+'_keys.distinct("_id");';
+                db.eval(query, function(err, result){
+                    var columns = new Array();
+                    for(i=0;i<result.length;i++){
+                      if (result[i]!="_id") {
+                        var column = {id: i, text: result[i]};
+                        columns.push(column);
+                      }                     
+                    }
+                    db.close();
+                    res.json(columns);
+                });
+            }
+        });
+      }
 });
 
-reponseJson =
+var responseJson =
 {
     "status": true
 }
@@ -38,10 +56,23 @@ reponseJson =
 * */
 
 router.post('/setDataSet',function(req,res,next){
-    console.log(req.body);
-    console.log(req.rawBody);
-    res.json(reponseJson)
-})
-
+     if (!req.session.loggedIn || !req.session.tableSet) {
+        responseJson.status=false;
+        res.json(responseJson);
+     } else {
+        //console.log(req.body.perfColId);
+        //console.log(req.body["selectedCols[]"]);
+        router.sess = req.session;
+        router.sess.perfSet = true;
+        router.sess.performance=req.body.perfColId;
+        var columns = new Array();
+        for(var i=0;i<req.body["selectedCols[]"].length;i++){
+            columns.push(parseInt(req.body["selectedCols[]"][i]))
+        }
+        //console.log(columns);
+        router.sess.columns=columns;
+        res.json(responseJson)
+     }
+});
 
 module.exports = router;
