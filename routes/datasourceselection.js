9ffca40142b26d33e9df288adc64218c2e48d1de
Ahmed <mahmedk91@gmail.com>
2016-01-16 04:16:32 +0530

    Delete project service - Deleted the project of a user and the tables associated with it. API call - http://localhost:3000/datasourceselection/deleteProject Type - POST data = {   "selectedProject":"project_name" } response either true or false with error message.

--- routes/datasourceselection.js
+++ routes/datasourceselection.js
@@ -1,12 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('morphologicalrecommender', server);
+
 /* GET start start page. */
 router.get('/', function(req, res, next) {
     if (req.session.loggedIn) {
         res.render('datasourceselection',{uploaderror:"false", data: req.session.userId});
     } else {
         res.render('start');
+    }
+});
+
+router.post('/deleteProject',function(req,res,next){
+    var reponseJson = {"status": true};
+    if (req.session.loggedIn) {
+        db.open(function(err, db) {
+            if(!err) {
+                var projects = db.collection("projects");
+                projects.find({userId: req.session.userId, name: req.body.selectedProject}).toArray(function(err, docs) {
+                    if (docs.length==1) {
+                        var table=db.collection(docs[0]["table"]);
+                        var table_keys=db.collection(docs[0]["table"]+"_keys");
+                        table.drop(function(err, respose1){
+                            table_keys.drop(function(err, response2){
+                                projects.deleteOne({userId: req.session.userId, name: req.body.selectedProject}, function(err, response3){
+                                    db.close();
+                                    res.json(reponseJson);
+                                });
+                            });
+                        });
+                    } else{
+                        responseJson.status=false;
+                        responseJson.message="Project not found or multiple copies of project exist";
+                        db.close();
+                        res.json(responseJson);
+                    }
+                });
+            }
+        });
+    } else {
+        responseJson.status=false;
+        responseJson.message="You are not logged in!";
+        res.json(responseJson);
     }
 });
 
