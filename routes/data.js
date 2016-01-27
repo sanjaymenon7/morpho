8b8c71498662c6af89880914fc0a7af5d3534e53
Ahmed <mahmedk91@gmail.com>
2016-01-27 13:54:50 +0530

    Export data in JSON and CSV backend. Please do an npm update

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var configVariables= require('../configVariables');
+var json2csv = require('json2csv');
+var fs = require('fs');
 
 var responseJson =
 {
@@ -19,6 +21,20 @@ var responseJson =
 router.get('/', function(req, res){
   var file = configVariables.configLines.uploadDestination+req.query.filename;
   res.download(file);
+});
+
+router.get('/csvFile', function(req, res){
+    var json = require(configVariables.configLines.uploadDestination+req.query.filename);
+    var fields = req.session.selectedCols;
+    json2csv({ data: json, fields: fields }, function(err, csv) {
+        if (err) console.log(err);
+        fs.writeFile(configVariables.configLines.uploadDestination+req.query.filename+'.csv', csv, function(err) {
+            if (err) throw err;
+            var file = configVariables.configLines.uploadDestination+req.query.filename+".csv";
+            res.download(file);
+        });
+        
+    });
 });
 
 module.exports = router;
